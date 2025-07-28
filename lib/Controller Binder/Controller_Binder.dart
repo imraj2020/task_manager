39f67c0ae6478203b119496b398336d7d47283e5
imraj2020 <imraj5050@gmail.com>
2025-07-28 23:26:21 +0600

    UpdateProfile with getx

--- lib/Controller Binder/Controller_Binder.dart
+++ lib/Controller Binder/Controller_Binder.dart
@@ -1,11 +1,19 @@
 import 'package:get/get.dart';
+import 'package:task_manager/Controller/cancelled_task_list_controller.dart';
+import '../Controller/completed_task_list_controller.dart';
 import '../Controller/new_task_list_controller.dart';
+import '../Controller/progress_task_list_controller.dart';
 import '../Controller/task_count_summary_controller.dart';
+import '../Controller/update_profile_controller.dart';
 
 class ControllerBinder extends Bindings {
   @override
   void dependencies() {
-    Get.put(NewTaskListController());
     Get.put(TaskCountSummaryController());
+    Get.put(NewTaskListController());
+    Get.put(ProgressTaskListController());
+    Get.put(CancelledTaskListController());
+    Get.put(CompletedTaskListController());
+    Get.put(UpdateProfileController());
   }
 }
---
+++ lib/Controller/update_profile_controller.dart
@@ -0,0 +1,128 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:task_manager/Controller/Auth_controller.dart';
+import 'package:task_manager/Model/User_Model.dart';
+import 'package:task_manager/Network/network_caller.dart';
+import 'package:task_manager/ui/utils/urls.dart';
+
+import '../widget/Snackbar_Messages.dart';
+
+class UpdateProfileController extends GetxController {
+  final email = TextEditingController();
+  final firstName = TextEditingController();
+  final lastName = TextEditingController();
+  final phone = TextEditingController();
+  final password = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  bool _obscurePassword = true;
+  bool _isLoading = false;
+  XFile? selectedImage;
+  final imagePicker = ImagePicker();
+
+  bool get obscurePassword => _obscurePassword;
+
+  set obscurePassword(bool value) {
+    _obscurePassword = value;
+    update();
+  }
+
+  bool get isLoading => _isLoading;
+
+  @override
+  void onInit() {
+    final user = AuthController.userModel;
+    email.text = user?.email ?? '';
+    firstName.text = user?.firstName ?? '';
+    lastName.text = user?.lastName ?? '';
+    phone.text = user?.mobile ?? '';
+    super.onInit();
+  }
+
+  void togglePasswordVisibility() {
+    obscurePassword = !obscurePassword;
+    update();
+  }
+
+  Future<void> pickImage(BuildContext context) async {
+    final picked = await imagePicker.pickImage(source: ImageSource.gallery);
+    if (picked != null) {
+      final fileSizeInBytes = await picked.length();
+      final sizeKB = fileSizeInBytes / 1024;
+
+      if (sizeKB > 64) {
+        showSnackBarMessage(
+          context,
+          'Image is too large. Max allowed size is 64KB.',
+        );
+        return;
+      }
+      selectedImage = picked;
+    } else {
+      showSnackBarMessage(context, 'No image selected');
+    }
+    update();
+  }
+
+  Future<void> submit(BuildContext context) async {
+    if (!formKey.currentState!.validate()) return;
+    _isLoading = true;
+    update();
+
+    Uint8List? imageBytes;
+    final body = {
+      'email': email.text.trim(),
+      'firstName': firstName.text.trim(),
+      'lastName': lastName.text.trim(),
+      'mobile': phone.text.trim(),
+    };
+
+    if (password.text.isNotEmpty) {
+      body['password'] = password.text;
+    }
+
+    if (selectedImage != null) {
+      imageBytes = await selectedImage!.readAsBytes();
+      body['photo'] = base64Encode(imageBytes);
+    }
+
+    final response = await networkCaller.postRequest(
+      url: urls.UpdateProfileUrl,
+      body: body,
+    );
+
+    if (response.isSuccess) {
+      final updatedUser = UserModel(
+        id: AuthController.userModel!.id,
+        email: email.text.trim(),
+        firstName: firstName.text.trim(),
+        lastName: lastName.text.trim(),
+        mobile: phone.text.trim(),
+        photo: imageBytes == null
+            ? AuthController.userModel!.photo
+            : base64Encode(imageBytes),
+      );
+      await AuthController.updateUserData(updatedUser);
+      password.clear();
+      showSnackBarMessage(context, 'Profile updated');
+    } else {
+      showSnackBarMessage(context, response.errorMessage ?? '');
+    }
+    _isLoading = false;
+    update();
+  }
+
+  @override
+  void onClose() {
+    email.dispose();
+    firstName.dispose();
+    lastName.dispose();
+    phone.dispose();
+    password.dispose();
+    super.onClose();
+  }
+}
