7a4fa48e63a44bc3ecb3182023f91a00591e0e27
imraj2020 <imraj5050@gmail.com>
2025-07-29 12:33:50 +0600

    Sign in controller with getx

--- lib/Controller Binder/Controller_Binder.dart
+++ lib/Controller Binder/Controller_Binder.dart
@@ -3,12 +3,14 @@ import 'package:task_manager/Controller/cancelled_task_list_controller.dart';
 import '../Controller/completed_task_list_controller.dart';
 import '../Controller/new_task_list_controller.dart';
 import '../Controller/progress_task_list_controller.dart';
+import '../Controller/sign_in_controller.dart';
 import '../Controller/task_count_summary_controller.dart';
 import '../Controller/update_profile_controller.dart';
 
 class ControllerBinder extends Bindings {
   @override
   void dependencies() {
+    Get.put(SignInController());
     Get.put(TaskCountSummaryController());
     Get.put(NewTaskListController());
     Get.put(ProgressTaskListController());
---
+++ lib/Controller/sign_in_controller.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import '../Model/User_Model.dart';
+import '../Network/network_caller.dart';
+import '../ui/screens/EmailVarification.dart';
+import '../ui/screens/SignUpScreen.dart';
+import '../ui/screens/main_navbar_screen.dart';
+import '../ui/utils/urls.dart';
+import '../widget/Snackbar_Messages.dart';
+import 'Auth_controller.dart';
+
+class SignInController extends GetxController {
+
+  bool _isLoading = false;
+  String? errorMessage;
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _obscurePassword = true;
+  bool get obscurePassword => _obscurePassword;
+  bool get isLoading => _isLoading;
+  bool get isObscurePassword => _obscurePassword;
+  TextEditingController get emailController => _emailController;
+  TextEditingController get passwordController => _passwordController;
+  GlobalKey<FormState> get formKey => _formKey;
+
+  set obscurePassword(bool value) {
+    _obscurePassword = value;
+    update();
+  }
+
+
+  void toggleObscurePassword() {
+    _obscurePassword = !_obscurePassword;
+    update();
+  }
+  void onTapForgetPassword(BuildContext context) {
+    Navigator.pushNamed(context, Emailvarification.name);
+  }
+
+  void onTapSignUpButton(BuildContext context) {
+    Navigator.pushNamed(context, SignUpScreen.name);
+  }
+
+  Future<void> signIn(BuildContext context) async {
+    _isLoading = true;
+    update();
+
+    Map<String, String> requestBody = {
+      "email": _emailController.text.trim(),
+      "password": _passwordController.text,
+    };
+    NetworkResponse response = await networkCaller.postRequest(
+      url: urls.LoginUrl,
+      body: requestBody,
+      isFromLogin: true,
+    );
+
+    if (response.isSuccess) {
+      UserModel userModel = UserModel.fromJson(response.body!['data']);
+      String token = response.body!['token'];
+
+      await AuthController.saveUserData(userModel, token);
+       showSnackBarMessage(context, 'Login successful');
+
+      await Navigator.pushNamedAndRemoveUntil(
+        context,
+        MainNavbarScreen.name,
+            (predicate) => false,
+      );
+      _isLoading = false;
+      update();
+    } else {
+      showSnackBarMessage(context, response.errorMessage!);
+    }
+
+    _isLoading = false;
+    update();
+
+  }
+
+
+}
